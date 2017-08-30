452e3d2c66ef22976107e96620b0e7a13121dfeb
Mike Crittenden <mikecrittenden@gmail.com>
2017-08-30 16:32:22 -0400

    Move everything to app directory

--- cache/compiled/files/c80d96b13e38a8bafd9324ee6a6121d3.yaml.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
-    'filename' => '/var/www/user/config/streams.yaml',
-    'modified' => 1503945480,
-    'data' => [
-        
-    ]
-];
