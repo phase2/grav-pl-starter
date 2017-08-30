e9b37d0cbf9aea59cd0ddffdeb59801ef2deb6c1
Mike Crittenden <mikecrittenden@gmail.com>
2017-08-30 17:21:05 -0400

    Copy instead of symlink

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -8,6 +8,9 @@ npm run compile
 
 # Symlink the bits we need into the Grav theme.
 cd ../../../
-ln -s vendor/phase2/pattern-lab-starter/fonts app/user/themes/gravpl/fonts || true
-ln -s vendor/phase2/pattern-lab-starter/dest app/user/themes/gravpl/dest || true
-ln -s vendor/phase2/pattern-lab-starter/source/_patterns app/user/themes/gravpl/templates/_patterns || true
+rm -rf ./app/user/themes/gravpl/fonts
+rm -rf ./app/user/themes/gravpl/dest
+rm -rf ./app/user/themes/gravpl/templates/_patterns
+cp -r vendor/phase2/pattern-lab-starter/fonts ./app/user/themes/gravpl/fonts || true
+cp -r vendor/phase2/pattern-lab-starter/dest ./app/user/themes/gravpl/dest || true
+cp -r vendor/phase2/pattern-lab-starter/source/_patterns ./app/user/themes/gravpl/templates/_patterns || true
