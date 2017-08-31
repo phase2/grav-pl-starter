c5bec205daf9506d07e845920b65910b5de91d8c
Mike Crittenden <mikecrittenden@gmail.com>
2017-08-30 21:23:36 -0400

    Readability updates for compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -6,11 +6,11 @@ npm install
 npm run setup
 npm run compile
 
-# Symlink the bits we need into the Grav theme.
+# Copy the bits we need into the Grav theme.
 cd ../../../
 rm -rf ./app/user/themes/gravpl/fonts
 rm -rf ./app/user/themes/gravpl/dest
 rm -rf ./app/user/themes/gravpl/templates/_patterns
-cp -r vendor/phase2/pattern-lab-starter/fonts ./app/user/themes/gravpl/fonts || true
-cp -r vendor/phase2/pattern-lab-starter/dest ./app/user/themes/gravpl/dest || true
-cp -r vendor/phase2/pattern-lab-starter/source/_patterns ./app/user/themes/gravpl/templates/_patterns || true
+cp -r vendor/phase2/pattern-lab-starter/fonts app/user/themes/gravpl/fonts
+cp -r vendor/phase2/pattern-lab-starter/dest app/user/themes/gravpl/dest
+cp -r vendor/phase2/pattern-lab-starter/source/_patterns app/user/themes/gravpl/templates/_patterns
